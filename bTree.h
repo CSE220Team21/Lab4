488d558786d3dd86ea050bf65993a6f0542e9a43
LevenBravo <charles.j.crow@gmail.com>
2014-04-10 16:22:32 -0700

    Create bTree.h

---
+++ bTree.h
@@ -0,0 +1,21 @@
+//
+//Mike Mayr (MMayr15), Chalie Crow (LevenBravo)
+//Team 21
+//bTree
+//
+
+#include "common.h"
+#include "token.h"
+#include "linkedList.h"
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream.h>
+
+using namespace std;
+
+class bTree
+{
+  
+}
